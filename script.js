8791861b026e6195662710a12d7645a866172e5f
JP <101474738+frutata@users.noreply.github.com>
2022-03-28 19:01:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function hide(element) {
+    element.remove();
+}
+
+function turnOff(element) {
+    element.innerText = "Sign Out";
+}
+
+function likeAlert() {
+    alert("Ninja was liked");
+}
